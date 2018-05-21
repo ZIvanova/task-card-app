2cc448f02ae92ef05bd39832f4e1185f2f60c799
ZIvanova <zornitsa.iw@gmail.com>
2018-05-21 17:44:46 +0300

    Last funcs + styling

--- Angular2/app/app.commponent.ts
+++ Angular2/app/app.commponent.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
 
+import { Task } from './model/task';
+
 @Component({
+    moduleId: module.id,
     selector: 'my-app',
-    template: '<h1>Skeleton Project</h1>'
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
 
-export class AppComponent { }
+export class AppComponent { 
+    // task1: Task = {
+    //     content: "Learn to code",
+    //     completed: false
+    // }
+
+    private tasks: Task[] = [];
+    private currentTask = new Task(null,false);
+
+    addTask(num: number, str?: string) {
+        let task = new Task(this.currentTask.content, this.currentTask.completed);
+        this.tasks.push(task);
+        this.currentTask.content = null;
+    }
+    // addTask():string {
+    //     return "tht";
+    // }
+}
--- Angular2/app/app.module.js
+++ Angular2/app/app.module.js
@@ -10,14 +10,22 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
+var forms_1 = require('@angular/forms');
 var app_commponent_1 = require('./app.commponent');
+var card_component_1 = require('./card/card.component');
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
-            declarations: [app_commponent_1.AppComponent],
+            imports: [
+                platform_browser_1.BrowserModule,
+                forms_1.FormsModule
+            ],
+            declarations: [
+                app_commponent_1.AppComponent,
+                card_component_1.CardComponent
+            ],
             bootstrap: [app_commponent_1.AppComponent]
         }), 
         __metadata('design:paramtypes', [])
